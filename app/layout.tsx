a527a7fced12875f56f3cd4464c2131bd6a79e9a
Vlad <pugachev.2013@yandex.ru>
2026-02-20 10:06:22 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,21 @@
+// app/layout.tsx
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+
 export default function RootLayout({
   children,
+  params, // Получаем params из маршрута
 }: {
   children: React.ReactNode;
+  params?: { lang: string };
 }) {
+  // По умолчанию 'ru', но можно взять из params
+  const lang = params?.lang || 'ru';
+
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang={lang}>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
